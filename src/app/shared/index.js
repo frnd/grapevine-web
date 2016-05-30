3232e5f2c3d18a05429f6f20f1fd9bc389ffa008
Fernando González <fgo@outlook.com>
2016-05-30 17:28:24 +0200

    remove compiled js added by error by intellij

--- src/app/shared/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('./message.model'));
-__export(require('./messagePreview.model'));
-//# sourceMappingURL=index.js.map
